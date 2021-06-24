1342ed025a1a0132c763bf2823eb0ae2e47fd21e
Manuel <manuel.pp2209@gmail.com>
2021-06-23 19:45:52 -0500

    'Modificacdo'

--- grafo.js
+++ grafo.js
@@ -1,4 +1,7 @@
 // Variables 
+
+
+
 let table = document.getElementById('grafo-table');
 let button = document.getElementById('ObtenerDatos');
 let resultado = document.getElementById('result');
@@ -19,7 +22,7 @@ function grafoButton() {
 
 
     let div = ``;
-    
+
     for (let i = 0; i < verValue; i++) {
         div += `<tr>`;
         for (let j = 0; j < arisValue; j++) {
@@ -29,7 +32,7 @@ function grafoButton() {
 
     }
 
-  
+
 
     table.innerHTML = div;
     for (let i = 0; i < verValue; i++) {
@@ -73,6 +76,8 @@ function obtenerDatos() {
     pintarGrafo(array);
     console.log(valorAristas(array));
 
+    var blob = new Blob([`${array}`], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "testfile1.txt");
     return array;
 
 
@@ -98,14 +103,14 @@ function noDirigido(array) {
 
 }
 
-function valorAristas(array) { 
+function valorAristas(array) {
 
     let valor = [];
     for (let i = 0; i < verValue; i++) {
         for (let j = i; j < verValue; j++) {
-            if (array[i][j] > 0 ) {
+            if (array[i][j] > 0) {
                 valor.push(parseInt(array[i][j]));
-           
+
             }
         }
 
@@ -119,7 +124,7 @@ function edges(array, symmetrical) {
     if (symmetrical) {
         for (let i = 0; i < verValue; i++) {
             for (let j = i; j < verValue; j++) {
-                if (array[i][j] > 0 ) {
+                if (array[i][j] > 0) {
                     // cont += parseInt(array[i][j]);
                     cont++;
                 }
@@ -131,7 +136,7 @@ function edges(array, symmetrical) {
             for (let j = 0; j < arisValue; j++) {
                 if (array[i][j] > 0) {
                     // cont += parseInt(array[i][j]);
-                    cont ++;
+                    cont++;
                 }
             }
         }
@@ -148,22 +153,22 @@ function pintarGrafo(array) {
     let posGrafo = '<g  fill="#FFD700">';
     let temp1 = 1;
     let temp2 = 1;
-    let grafoArrayX= [];
-    let grafoArrayY= [];
+    let grafoArrayX = [];
+    let grafoArrayY = [];
     let posx;
     let posy;
-    let abecedario = [ 'A','B' ,'C' ,'D' ,'E', 'F' ,'G' ,'H' ,' I' ,'J' , 'K'];
+    let abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ' I', 'J', 'K'];
     for (let i = 1; i <= verValue; i++) {
         if (i % 2 == 0) {
-            posx = temp1 * 20; 
+            posx = temp1 * 15;
             posy = 20;
             posGrafo += `<circle cx="${posx}%" cy="${posy}%" r="3"> </circle>`;
             grafoArrayX.push([posx]);
             grafoArrayY.push([posy]);
-            
+
             temp1++;
         } else {
-            posx = temp2 * 20; 
+            posx = temp2 * 20;
             posy = 40;
             posGrafo += `<circle cx="${posx}%" cy="${posy}%" r="3"> </circle>`;
             grafoArrayX.push([posx]);
@@ -179,34 +184,34 @@ function pintarGrafo(array) {
     let letras = '<g fill="#000000" font-size="1.8" text-anchor="middle">';
     for (let i = 0; i < verValue; i++) {
         letras += `<text x="${grafoArrayX[i]}%" y="${grafoArrayY[i]}%" dy="1">${abecedario[i]}</text>`
-        
+
     }
 
-    letras += '</g>'; 
-    let lineasPos= [];
+    letras += '</g>';
+    let lineasPos = [];
     let lineas = '<g  stroke-width="0.1">';
     // if (symmetrical) {
-        let value = valorAristas(array);
-        let posText = 0;
-        for (let i = 0; i < verValue; i++) {
-            for (let j = i+1; j < verValue; j++) {
-                console.log(value[i]);
-                if (array[i][j] > 0) {
-                     lineas += `<line stroke="#00FF00" id = ${i},${j} x1="${grafoArrayX[i]}%" y1="${grafoArrayY[i]}%" x2="${grafoArrayX[j]}%" y2="${grafoArrayY[j]}%"/>`
+    let value = valorAristas(array);
+    let posText = 0;
+    for (let i = 0; i < verValue; i++) {
+        for (let j = i + 1; j < verValue; j++) {
+            console.log(value[i]);
+            if (array[i][j] > 0) {
+                lineas += `<line stroke="#00FF00" id = ${i},${j} x1="${grafoArrayX[i]}%" y1="${grafoArrayY[i]}%" x2="${grafoArrayX[j]}%" y2="${grafoArrayY[j]}%"/>`
 
-                     
-                     lineas += `<text x="${((parseInt(grafoArrayX[i])+parseInt(grafoArrayX[j]))/2)}%" y="${(parseInt(grafoArrayY[i])+parseInt(grafoArrayY[j]))/2}%" font-family="Verdana" font-size="1" alignment-baseline="text-before-edge" fill="#000">
+
+                lineas += `<text x="${((parseInt(grafoArrayX[i]) + parseInt(grafoArrayX[j])) / 2)}%" y="${(parseInt(grafoArrayY[i]) + parseInt(grafoArrayY[j])) / 2}%" font-family="Verdana" font-size="1" alignment-baseline="text-before-edge" fill="#000">
                             ${value[posText++]}
                  </text>`;
-                    }
             }
-
         }
+
+    }
     // }
-        
+
     lineas += '</g>';
-    svg.innerHTML = grafo + letras + lineas ;
-   
+    svg.innerHTML = grafo + letras + lineas;
+
 }
 
 // if (i == 0) {
