0f7ec817df452456974de6b2a5c70bc7d7c93de3
Manuel <manuel.pp2209@gmail.com>
2021-06-25 13:42:01 -0500

    Actualizacion del SiSEc

--- grafo.js
+++ grafo.js
@@ -1,7 +1,4 @@
 // Variables 
-
-
-
 let table = document.getElementById('grafo-table');
 let button = document.getElementById('ObtenerDatos');
 let resultado = document.getElementById('result');
@@ -13,43 +10,73 @@ let numNodos = 0;
 let symmetrical;
 let ed = 0;
 
+// define algunas variables necesarias
+var cx = 50;
+var cy = 20;
+var r = 13;
+var lados = 0;
 
+// // la función points
+// function points(cx, cy, r, lados) {
+//     var a = 360 / lados;
+//     // inicia la variable points
+//     var points = (cx + r) + "," + cy + " ";
+//     // calcula el resto de los puntos
+//     for (var i = 1; i <= lados; i++) {
+//         var aRad = (Math.PI / 180) * (a * i);
+//         Xp = cx + r * Math.cos(aRad);
+//         Yp = cy + r * Math.sin(aRad);
+//         console.log(Xp,Yp);
+//         console.log(points += Xp + "," + Yp + " ");
+//     }
+//     // devuelve la variable points			
+//     return points;
+// }
+
+// console.log(points(cx, cy, r, lados));
+// // busca el elemento #poligono
+
+// var poligono = document.getElementById("poligono");
+// modifica el valor del atributo points utilizando la función points.
+// poligono.setAttribute("points", points(cx, cy, r, lados));
 function grafoButton() {
     verValue = ver.value;
     arisValue = ver.value;
     document.getElementById('cont-1').innerHTML = 'Ingrese los valores del grafo:';
 
 
+    if (verValue < 10) {
 
-    let div = ``;
+        let div = ``;
 
-    for (let i = 0; i < verValue; i++) {
-        div += `<tr>`;
-        for (let j = 0; j < arisValue; j++) {
-            div += `<td><input disabled id = ${i},${j} type="text"></td>`;
+        for (let i = 0; i < verValue; i++) {
+            div += `<tr>`;
+            for (let j = 0; j < arisValue; j++) {
+                div += `<td><input disabled id = ${i},${j} type="text"></td>`;
+            }
+            div += `</tr>`;
+
         }
-        div += `</tr>`;
+
+
+
+        table.innerHTML = div;
+        for (let i = 0; i < verValue; i++) {
+            for (let j = i + 1; j < arisValue; j++) {
+                let vertice = document.getElementById(`${i},${j}`);
+                vertice.removeAttribute("disabled");
+            }
+        }
+        button.removeAttribute("disabled");
 
     }
-
-
-
-    table.innerHTML = div;
-    for (let i = 0; i < verValue; i++) {
-        for (let j = i + 1; j < arisValue; j++) {
-            let vertice = document.getElementById(`${i},${j}`);
-            vertice.removeAttribute("disabled");
-        }
-    }
-    button.removeAttribute("disabled");
-
 }
 
 
 
 function obtenerDatos() {
 
-
+    lados = verValue;
     let array = [];
 
 
@@ -75,9 +102,8 @@ function obtenerDatos() {
 
     pintarGrafo(array);
     console.log(valorAristas(array));
+    // txtGrafo(array);
 
-    var blob = new Blob([`${array}`], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, "testfile1.txt");
     return array;
 
 
@@ -158,32 +184,51 @@ function pintarGrafo(array) {
     let posx;
     let posy;
     let abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ' I', 'J', 'K'];
-    for (let i = 1; i <= verValue; i++) {
-        if (i % 2 == 0) {
-            posx = temp1 * 15;
-            posy = 20;
-            posGrafo += `<circle cx="${posx}%" cy="${posy}%" r="3"> </circle>`;
-            grafoArrayX.push([posx]);
-            grafoArrayY.push([posy]);
 
-            temp1++;
-        } else {
-            posx = temp2 * 20;
-            posy = 40;
-            posGrafo += `<circle cx="${posx}%" cy="${posy}%" r="3"> </circle>`;
-            grafoArrayX.push([posx]);
-            grafoArrayY.push([posy]);
-            temp2++;
+    if (verValue < 5) {
+        for (let i = 1; i <= verValue; i++) {
+            if (i % 2 == 0) {
+                posx = temp1 * 30;
+                posy = 20;
+                posGrafo += `<circle cx="${posx}%" cy="${posy}%" r="3"> </circle>`;
+                grafoArrayX.push([posx]);
+                grafoArrayY.push([posy]);
+
+                temp1++;
+            } else {
+                posx = temp2 * 25;
+                posy = 40;
+                posGrafo += `<circle cx="${posx}%" cy="${posy}%" r="3"> </circle>`;
+                grafoArrayX.push([posx]);
+                grafoArrayY.push([posy]);
+                temp2++;
+            }
+        }
+    } else {
+        var a = 360 / lados;
+        // inicia la variable points
+        var points = (cx + r) + "," + cy + " ";
+        // calcula el resto de los puntos
+        for (var i = 1; i <= lados; i++) {
+            var aRad = (Math.PI / 180) * (a * i);
+            Xp = cx + r * Math.cos(aRad);
+            Yp = cy + r * Math.sin(aRad);
+            posGrafo += `<circle cx="${Xp}%" cy="${Yp}%" r="3"> </circle>`;
+            points += Xp + "," + Yp + " ";
+            grafoArrayX.push([Xp]);
+            grafoArrayY.push([Yp]);
         }
     }
+
     posGrafo += '</g>';
     let grafo = `${posGrafo}`;
 
     // svg.innerHTML = grafo;
 
     let letras = '<g fill="#000000" font-size="1.8" text-anchor="middle">';
+    
     for (let i = 0; i < verValue; i++) {
-        letras += `<text x="${grafoArrayX[i]}%" y="${grafoArrayY[i]}%" dy="1">${abecedario[i]}</text>`
+        letras += `<text  x="${grafoArrayX[i]}%" y="${grafoArrayY[i]}%" dy="1">${abecedario[i]}</text>`
 
     }
 
@@ -197,10 +242,13 @@ function pintarGrafo(array) {
         for (let j = i + 1; j < verValue; j++) {
             console.log(value[i]);
             if (array[i][j] > 0) {
-                lineas += `<line stroke="#00FF00" id = ${i},${j} x1="${grafoArrayX[i]}%" y1="${grafoArrayY[i]}%" x2="${grafoArrayX[j]}%" y2="${grafoArrayY[j]}%"/>`
+                
+                var randomColor = Math.floor(Math.random()*16777215).toString(16);
+                console.log(randomColor);
+                lineas += `<line stroke="#${randomColor}" id = ${i},${j} x1="${grafoArrayX[i]}%" y1="${grafoArrayY[i]}%" x2="${grafoArrayX[j]}%" y2="${grafoArrayY[j]}%"/>`
 
 
-                lineas += `<text x="${((parseInt(grafoArrayX[i]) + parseInt(grafoArrayX[j])) / 2)}%" y="${(parseInt(grafoArrayY[i]) + parseInt(grafoArrayY[j])) / 2}%" font-family="Verdana" font-size="1" alignment-baseline="text-before-edge" fill="#000">
+                lineas += `<text fill="#${randomColor}" x="${((parseInt(grafoArrayX[i]) + parseInt(grafoArrayX[j])) / 2)}%" y="${(parseInt(grafoArrayY[i]) + parseInt(grafoArrayY[j])) / 2}%" font-family="Verdana" font-size="1" alignment-baseline="text-before-edge" fill="#000">
                             ${value[posText++]}
                  </text>`;
             }
@@ -214,11 +262,10 @@ function pintarGrafo(array) {
 
 }
 
-// if (i == 0) {
-//     posGrafo += `<circle cx="${i*10}" cy="=10" r="3"> </circle> `; 
-// }
-// else if (i % 2 == 0) {
-// } else {
-//     posGrafo +=`<circle cx="${temp*10}" cy="=40" r="3"> </circle>`;
-//     temp++;
-// }
+function txtGrafo() {
+    array = obtenerDatos();
+    var blob = new Blob([`[ciudades : ${verValue}] \n Hi`], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "Grafo.txt");
+
+
+}
